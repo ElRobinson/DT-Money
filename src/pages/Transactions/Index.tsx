645c16d5b023af9cbff54f3a409d036af9e5a13b
Luis Felipe Trein Robinson <luisfelipe.robinson@gmail.com>
2023-03-30 17:17:44 -0300

    feat: formatting values

--- src/pages/Transactions/Index.tsx
+++ src/pages/Transactions/Index.tsx
@@ -3,6 +3,7 @@ import { Header } from "../../components/Header";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
 import { SearchForm } from "../../components/SearchForm";
 import { Summary } from "../../components/Summary";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 interface Transaction {
   id: number;
@@ -32,11 +33,12 @@ export function Transactions() {
                   <td width="50%">{transaction.description}</td>
                   <td>
                     <PriceHighlight variant={transaction.type}>
-                      {transaction.price}
+                      {transaction.type === 'outcome' && '- '}
+                      {priceFormatter.format(transaction.price)}
                     </PriceHighlight>
                   </td>
                   <td>{transaction.category}</td>
-                  <td>{transaction.createdAt}</td>
+                  <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
                 </tr>
               )
             })}
